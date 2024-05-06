348d1a3952d2358870f9ae08f2453ff1134fc807
witalomelo <witalovmelo@gmail.com>
2024-05-06 15:29:48 -0300

    gerando relatorios e playback

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false, 
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    }
   },
 });
